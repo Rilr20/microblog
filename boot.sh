9e8ae9206fc93cf40e36e9a5469580e737f1f7bf
rilr20 <nrrl99@gmail.com>
2022-11-23 20:34:10 +0100

    boot changes

--- boot.sh
+++ boot.sh
@@ -11,4 +11,4 @@ while true; do
     sleep 5
 done
 
-exec gunicorn -b :5000 --access-logfile - --error-logfile - microblog:app
+exec gunicorn -b :5000 --access-logfile - --error-logfile - -c gunicorn_config.py microblog:app
